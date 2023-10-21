e01adf4edb5c4ea1c5db03a9b817d5d791398ac7
Jason Hudgins <jason@hudgins.co>
2023-10-20 19:01:34 -0500

    cleaned up the address data class

--- pylotus_rpc/types/Address.py
+++ pylotus_rpc/types/Address.py
@@ -1,30 +1,12 @@
 class Address:
-    # Address is a class that represents an address in the Filecoin network.
-    def __init__(self, address_value: str):
+    """
+    Represents an address in the Filecoin network.
+    """
+
+    def __init__(self, address: str):
         """
         Initializes a new Address object.
 
-        :param address_value: A string representing the value of the address.
+        :param address: A string representing the value of the address.
         """
-        self.address_value = address_value
-
-    def is_id_address(self):
-        """
-        Checks if the address is an ID-based address.
-
-        :return: True if it's an ID-based address, False otherwise.
-        """
-        # Implement the logic to check if the address is an ID-based address.
-        # For example, you can check if it starts with 'f' for mainnet addresses.
-        return self.address_value.startswith('f')
-
-    def is_actor_address(self):
-        """
-        Checks if the address is an actor-based address.
-
-        :return: True if it's an actor-based address, False otherwise.
-        """
-        # Implement the logic to check if the address is an actor-based address.
-        # You may need to define specific prefixes for actor addresses.
-        # For example, 't' for testnet actor addresses.
-        return self.address_value.startswith('t')
+        self.address_value = address
