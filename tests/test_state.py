43defa7a684afde2210c7d2d96cec089a95d5666
Jason Hudgins <jason@hudgins.co>
2023-12-25 14:07:59 -0500

    test scripts now initialize from the LOTUS_GATEWAY variable

--- tests/test_state.py
+++ tests/test_state.py
@@ -60,9 +60,7 @@ def setup_filfox_connector():
 
 @pytest.fixture
 def setup_connector():
-    # TODO - use environment variable for host
-    # host = "http://lotus.filforge.io:1234/rpc/v0"
-    host = "https://filfox.info/rpc/v1"
+    host = os.environ.get('LOTUS_GATEWAY', 'https://filfox.info/rpc/v1')
     return HttpJsonRpcConnector(host=host)
 
 
