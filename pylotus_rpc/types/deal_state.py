c625c3f4dc9c2eb075c39ca391bff822795e7876
Jason Hudgins <jason@hudgins.co>
2024-01-12 16:24:01 -0500

    added from_dict to DealState for initialiaztion

--- pylotus_rpc/types/deal_state.py
+++ pylotus_rpc/types/deal_state.py
@@ -5,3 +5,13 @@ class DealState:
     sector_start_epoch: int
     last_updated_epoch: int
     slash_epoch: int
+    verified_claim: int
+
+    @staticmethod
+    def from_dict(dct: dict) -> 'DealState':
+        return DealState(
+            sector_start_epoch=dct.get("SectorStartEpoch", -1),
+            last_updated_epoch=dct.get("LastUpdatedEpoch", -1),
+            slash_epoch=dct.get("SlashEpoch", -1),
+            verified_claim=dct.get("VerifiedClaim", 0)
+        )
