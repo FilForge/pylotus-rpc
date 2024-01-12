6d3df1f3e64ad2137841b6eabe4526d91d830ba2
Jason Hudgins <jason@hudgins.co>
2024-01-12 16:23:38 -0500

    added from_dict to DealProposal type for initialization

--- pylotus_rpc/types/deal_proposal.py
+++ pylotus_rpc/types/deal_proposal.py
@@ -14,3 +14,20 @@ class DealProposal:
     storage_price_per_epoch: int
     provider_collateral: int
     client_collateral: int
+
+    @staticmethod
+    def from_dict(dct: dict) -> 'DealProposal':
+        return DealProposal(
+            piece_cid=Cid.from_dict(dct["PieceCID"]),
+            piece_size=dct["PieceSize"],
+            verified_deal=dct["VerifiedDeal"],
+            client_addr=dct["Client"],
+            provider_addr=dct["Provider"],
+            label=dct["Label"],
+            start_epoch=dct["StartEpoch"],
+            end_epoch=dct["EndEpoch"],
+            storage_price_per_epoch=int(dct["StoragePricePerEpoch"]),
+            provider_collateral=int(dct["ProviderCollateral"]),
+            client_collateral=int(dct["ClientCollateral"])
+        )
+
